eef3afbd3c28e22c723f951860ca04cc6c16a991
Miguel FAUCHEUX <mfaucheu@f4r5s8.clusters.42paris.fr>
2025-09-10 14:49:16 +0200

    ex00 and ex01 good need testing errors

---
+++ ex00/main.py
@@ -0,0 +1,27 @@
+from S1E9 import Character, Stark
+
+
+def subject_tests():
+    Ned = Stark("Ned")
+    print(Ned.__dict__)
+    print(Ned.is_alive)
+    Ned.die()
+    print(Ned.is_alive)
+    print(Ned.__doc__)
+    print(Ned.__init__.__doc__)
+    print(Ned.die.__doc__)
+    print("---")
+    Lyanna = Stark("Lyanna", False)
+    print(Lyanna.__dict__)
+
+    hodor = Character("hodor")
+    print(hodor)
+
+
+def main():
+    subject_tests()
+    return 0
+
+
+if __name__ == "__main__":
+    main()
